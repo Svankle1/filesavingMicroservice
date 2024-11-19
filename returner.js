e34c1998313680ac1fa27a77e21bbf04b8452715
Svankle1 <146278205+Svankle1@users.noreply.github.com>
2024-11-19 13:20:26 -0800

    getting started with 0mq example code

---
+++ returner.js
@@ -0,0 +1,14 @@
+const zmq = require("zeromq")
+
+async function run() {
+  const sock = new zmq.Reply()
+
+  await sock.bind("tcp://127.0.0.1:3000")
+
+  for await (const [msg] of sock) {
+    console.log(msg.toString())
+    await sock.send('Responding to: ' + msg.toString())
+  }
+}
+
+run()
