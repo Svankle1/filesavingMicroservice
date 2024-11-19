939561df56c65e1a52a00031e942b4c0fe573145
Svankle1 <146278205+Svankle1@users.noreply.github.com>
2024-11-19 13:34:15 -0800

    starting to (to be honest, midlessly) iplement file reading

--- returner.js
+++ returner.js
@@ -1,4 +1,5 @@
 const zmq = require("zeromq");
+const fs = require('node:fs');
 
 async function run() {
   const sock = new zmq.Reply();
@@ -7,6 +8,29 @@ async function run() {
 
   for await (const [msg] of sock) {
     console.log(msg.toString());
+    fs.readFile('/.playlistSessionData', 'utf8', (err, data) => {
+      if (err) {
+        if (err.code = 'ENOENT'){ //No file exists, therefore create one
+          data = 'workTime';
+          fs.writeFile('/.playlistSessionData', content, err => {
+            if (err) { //ERROR
+              console.error(err);
+            } else {
+              // file written successfully
+            }
+          });
+        }
+      else{ //ERROR
+          console.error(err);
+          return;
+        }
+      }
+      console.log(data);
+    });
+
+
+
+
     await sock.send('Responding to: ' + msg.toString());
   }
 }
