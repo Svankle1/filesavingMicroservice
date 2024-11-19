949bde03983022b79ea58919d338fa75fbcaaaf8
Svankle1 <146278205+Svankle1@users.noreply.github.com>
2024-11-19 13:22:21 -0800

    ;

--- returner.js
+++ returner.js
@@ -1,14 +1,14 @@
-const zmq = require("zeromq")
+const zmq = require("zeromq");
 
 async function run() {
-  const sock = new zmq.Reply()
+  const sock = new zmq.Reply();
 
-  await sock.bind("tcp://127.0.0.1:3000")
+  await sock.bind("tcp://127.0.0.1:3000");
 
   for await (const [msg] of sock) {
-    console.log(msg.toString())
-    await sock.send('Responding to: ' + msg.toString())
+    console.log(msg.toString());
+    await sock.send('Responding to: ' + msg.toString());
   }
 }
 
-run()
+run();
